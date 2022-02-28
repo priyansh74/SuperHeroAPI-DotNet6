90637249d0761dbbe35c40a2cc645897b2af84e2
priyansh74 <priyanshjha99@gmail.com>
2022-02-28 11:31:25 +0530

    Added data validation

--- SuperHero.cs
+++ SuperHero.cs
@@ -1,13 +1,24 @@
-﻿namespace SuperHeroAPI
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperHeroAPI
 {
     public class SuperHero
-    { 
+    {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Only Alphabets are allowed.")]
         public string Name { get; set; } = string.Empty;
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Only Alphabets are allowed.")]
         public string FirstName { get; set; } = string.Empty;
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Only Alphabets are allowed.")]
         public string LastName { get; set; } = string.Empty;
-        public string Place { get; set; } = string.Empty;
-
-        
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Only Alphabets are allowed.")]
+        public string Place { get; set; } = string.Empty;   
     }
 }
